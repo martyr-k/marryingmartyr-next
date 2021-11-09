2fc7ffc277044445a0d5e8351a4d598d95afa488
martyrk <admin@martyrk.design>
2021-11-09 11:36:50 -0500

    create navbar

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -2,51 +2,58 @@ import { Container } from "react-bootstrap";
 import { Navbar } from "react-bootstrap";
 import { Nav } from "react-bootstrap";
 import Link from "next/link";
+import Image from "next/image";
 
 import useAuthenticatedClient from "hooks/useAuthenticatedClient";
+import monogram from "../public/imgs/monogram.png";
+import styles from "./styles/NavigationBar.module.css";
 
 const NavigationBar = () => {
-  const { user } = useAuthenticatedClient();
+  const { code } = useAuthenticatedClient();
 
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar className={styles.navbar} bg="black" expand="lg" variant="dark">
       <Container fluid>
         <Link href="/" passHref>
-          <Navbar.Brand>Demo App</Navbar.Brand>
+          <div className="cursor-pointer" style={{ height: "50px" }}>
+            <Image
+              src={monogram}
+              alt="wedding monogram"
+              height={50}
+              width={50}
+            />
+          </div>
         </Link>
         <Navbar.Toggle aria-controls="navbarDropdown" />
         <Navbar.Collapse id="navbarDropdown">
-          <Nav className="me-auto">
-            {["admin", "developer"].includes(user?.role) && (
-              <Link href="/events/new" passHref>
-                <Nav.Link>Create an Event</Nav.Link>
+          <Nav className="justify-content-center w-100">
+            <Link href="/" passHref>
+              <Nav.Link className={styles.navLink}>Home</Nav.Link>
+            </Link>
+            <Link href="/rsvp" passHref>
+              <Nav.Link className={styles.navLink}>RSVP</Nav.Link>
+            </Link>
+            <Link href="/rsvp" passHref>
+              <Nav.Link className={styles.navLink}>Our Story</Nav.Link>
+            </Link>
+            <Link href="/rsvp" passHref>
+              <Nav.Link className={styles.navLink}>FAQ</Nav.Link>
+            </Link>
+            <Link href="/rsvp" passHref>
+              <Nav.Link className={styles.navLink}>Registry</Nav.Link>
+            </Link>
+            <Link href="/rsvp" passHref>
+              <Nav.Link className={styles.navLink}>Wedding Party</Nav.Link>
+            </Link>
+            <Link href="/rsvp" passHref>
+              <Nav.Link className={styles.navLink}>Livestream</Nav.Link>
+            </Link>
+            {["admin"].includes(code?.role) && (
+              <Link href="/dashboard" passHref>
+                <Nav.Link>Dashboard</Nav.Link>
               </Link>
             )}
           </Nav>
-          <Navbar.Text className="ms-auto">
-            {user ? (
-              <>
-                <span className="me-3">
-                  Welcome,{" "}
-                  <Link href="/profile">
-                    <a>{user.email}</a>
-                  </Link>
-                </span>
-                <button className="btn btn-outline-danger" onClick={logout}>
-                  <i className="bi bi-box-arrow-right" />
-                </button>
-              </>
-            ) : (
-              <>
-                <Link href="/auth/login">
-                  <a className="me-2">Login</a>
-                </Link>
-                <Link href="/auth/signup">
-                  <a>Sign Up</a>
-                </Link>
-              </>
-            )}
-          </Navbar.Text>
         </Navbar.Collapse>
       </Container>
     </Navbar>
