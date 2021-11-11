f8e220e8df7b66e092bba9066280e06a55e3a94a
martyrk <admin@martyrk.design>
2021-11-11 13:33:31 -0500

    finalize rsvp for in-person guests api route

--- pages/api/rsvp.js
+++ pages/api/rsvp.js
@@ -9,15 +9,15 @@ const handler = nc();
 handler.use(dbConnect, secured).post(async (req, res) => {
   try {
     const { song, artist, confirmedGuests } = req.body;
-    // const code = await Code.findById(req.code._id);
+    const code = await Code.findById(req.code._id);
 
-    // code.rsvp = true;
-    // code.confirmedGuests = confirmedGuests;
-    // if (song || artist) {
-    //   code.song = `${req.body.artist} - ${req.body.song}`;
-    // }
+    code.rsvp = true;
+    code.confirmedGuests = confirmedGuests;
+    if (song || artist) {
+      code.song = `${req.body.artist} - ${req.body.song}`;
+    }
 
-    // await code.save();
+    await code.save();
 
     res.status(201).json({ status: "success" });
   } catch (error) {
