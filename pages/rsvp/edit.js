c890df0ba6c215a138399cc792d9684ac9c4431d
martyrk <admin@martyrk.design>
2021-11-17 13:11:36 -0500

    add virtual guests information on edit rsvp page

--- pages/rsvp/edit.js
+++ pages/rsvp/edit.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import Image from "next/image";
+import Link from "next/link";
 import { Button } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
@@ -112,81 +113,97 @@ const EditRSVP = () => {
           We can&apos;t wait for you to celebrate with us!
         </h1>
 
-        <form className="mt-5 w-75 mx-auto" onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-md-6">
-              <h2 className="lead mb-3 text-center text-md-start display-4">
-                Please confirm who will be attending:
-              </h2>
-              <div>
-                {code.invitedGuests.map((guest, index) => {
-                  return (
-                    <div className="input-group mb-2" key={index}>
-                      <div className="input-group-text">
+        {code.attendance === "in-person" && (
+          <form className="mt-5 w-75 mx-auto" onSubmit={handleSubmit}>
+            <div className="row">
+              <div className="col-md-6">
+                <h2 className="lead mb-3 text-center text-md-start display-4">
+                  Please confirm who will be attending:
+                </h2>
+                <div>
+                  {code.invitedGuests.map((guest, index) => {
+                    return (
+                      <div className="input-group mb-2" key={index}>
+                        <div className="input-group-text">
+                          <input
+                            className="form-check-input"
+                            type="checkbox"
+                            name={guest}
+                            value={guest}
+                            checked={checkedState[index]}
+                            onChange={() => handleOnChange(index)}
+                            aria-label="Checkbox for confirming guest attendance"
+                          />
+                        </div>
                         <input
-                          className="form-check-input"
-                          type="checkbox"
-                          name={guest}
+                          className="form-control"
+                          disabled
                           value={guest}
-                          checked={checkedState[index]}
-                          onChange={() => handleOnChange(index)}
-                          aria-label="Checkbox for confirming guest attendance"
+                          aria-label="Guest name"
                         />
                       </div>
-                      <input
-                        className="form-control"
-                        disabled
-                        value={guest}
-                        aria-label="Guest name"
-                      />
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-            <div className="col-md-6 d-flex flex-column justify-content-center">
-              <p className="lead mb-3">What song would make you dance?</p>
-              <div className="row g-1 mb-2">
-                <label className="col-lg-2" htmlFor="song">
-                  Song:
-                </label>
-                <div className="col-lg-10">
-                  <input
-                    className="form-control"
-                    type="text"
-                    id="song"
-                    name="song"
-                    value={song}
-                    onChange={handleSongChange}
-                  />
+                    );
+                  })}
                 </div>
               </div>
-              <div className="row g-1 mb-2">
-                <label className="col-lg-2" htmlFor="artist">
-                  Artist:
-                </label>
-                <div className="col-lg-10">
-                  <input
-                    className="form-control"
-                    type="text"
-                    id="artist"
-                    name="artist"
-                    value={artist}
-                    onChange={handleArtistChange}
-                  />
+              <div className="col-md-6 d-flex flex-column justify-content-center">
+                <p className="lead mb-3">What song would make you dance?</p>
+                <div className="row g-1 mb-2">
+                  <label className="col-lg-2" htmlFor="song">
+                    Song:
+                  </label>
+                  <div className="col-lg-10">
+                    <input
+                      className="form-control"
+                      type="text"
+                      id="song"
+                      name="song"
+                      value={song}
+                      onChange={handleSongChange}
+                    />
+                  </div>
+                </div>
+                <div className="row g-1 mb-2">
+                  <label className="col-lg-2" htmlFor="artist">
+                    Artist:
+                  </label>
+                  <div className="col-lg-10">
+                    <input
+                      className="form-control"
+                      type="text"
+                      id="artist"
+                      name="artist"
+                      value={artist}
+                      onChange={handleArtistChange}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
+            <Button
+              className="mx-auto d-block mt-3 btn-burgundy"
+              type="submit"
+              size="lg"
+            >
+              Submit
+            </Button>
+          </form>
+        )}
+
+        {code.attendance === "virtual" && (
+          <div className="mt-5 w-75">
+            <h2>Reservation Confirmed!</h2>
+            <h3>Tune in at 1:30 PM on October 3, 2021.</h3>
           </div>
-          <Button
-            className="mx-auto d-block mt-3"
-            variant="secondary"
-            type="submit"
-            size="lg"
-          >
-            Submit
-          </Button>
-        </form>
+        )}
+
+        <div className="text-center mt-5">
+          <Link href="/registry" passHref>
+            <Button variant="secondary" size="lg">
+              Browse Registry
+            </Button>
+          </Link>
+        </div>
       </div>
     </PageLayout>
   );
