2ab62df6780916538bb6d352b10d68f6eb0050f7
martyrk <admin@martyrk.design>
2021-11-19 13:15:41 -0500

    add view invitees page

--- pages/api/invitees.js
+++ pages/api/invitees.js
@@ -6,7 +6,9 @@ import Code from "models/codes";
 
 const handler = nc();
 
-handler.use(dbConnect, secured, restricted("admin")).post(async (req, res) => {
+handler.use(dbConnect, secured, restricted("admin"));
+
+handler.post(async (req, res) => {
   try {
     const { inviteCode, attendance, invitedGuests, alias } = req.body;
 
@@ -20,6 +22,32 @@ handler.use(dbConnect, secured, restricted("admin")).post(async (req, res) => {
 
     res.status(201).json({
       status: "success",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failure",
+      message: error.message,
+    });
+  }
+});
+
+handler.get(async (req, res) => {
+  try {
+    const invitees = await Code.find();
+
+    const totalConfirmedAttendees = invitees.reduce((sum, invitee) => {
+      if (invitee.attendance === "in-person") {
+        return sum + invitee.confirmedGuests.length * 1;
+      }
+      return sum;
+    }, 0);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        invitees,
+        totalConfirmedAttendees,
+      },
     });
   } catch (error) {
     res.status(400).json({
--- pages/invitees/index.js
+++ pages/invitees/index.js
@@ -1,16 +1,88 @@
+import useSWR from "swr";
+import axios from "axios";
+import { Container } from "react-bootstrap";
+
 import PageLayout from "components/PageLayout";
 import LoadingSpinner from "components/LoadingSpinner";
 import useAuthenticatedClient from "hooks/useAuthenticatedClient";
 import styles from "styles/ViewInvitees.module.css";
+import { useAuthentication } from "contexts/AuthenticationContext";
+
+const fetcher = (url, token) => {
+  return axios
+    .get(url, {
+      headers: {
+        authorization: token.value,
+      },
+    })
+    .then((response) => {
+      return response.data;
+    });
+};
 
 const ViewInvitees = () => {
   const { isLoading } = useAuthenticatedClient("/rsvp", "admin");
+  const { token } = useAuthentication();
+  const { data, error } = useSWR(token && ["/api/invitees", token], fetcher);
+  // const { invitees, totalConfirmedAttendees } = data?.data;
 
   return isLoading ? (
     <LoadingSpinner />
   ) : (
-    <PageLayout>
-      <div className={styles.main}></div>
+    <PageLayout title="View Invitees">
+      <div className={styles.main}>
+        <Container className="py-4">
+          <div className="d-flex justify-content-between mb-5">
+            <div>
+              <h1 className="display-2">Wedding Invitees</h1>
+            </div>
+            <h4>{data?.data?.totalConfirmedAttendees} Confirmed Attendees</h4>
+          </div>
+
+          <table className={styles.table}>
+            {data?.data?.invitees.map((invitee) => {
+              return (
+                <tr className={styles.tableRow} key={invitee._id}>
+                  <td>
+                    <a
+                      className="familyAlias"
+                      data-id="<%= invitee._id %>"
+                      href="#"
+                    >
+                      {invitee.alias}
+                    </a>
+                    {invitee.email && <span> ✅</span>}
+                    {invitee.rsvp && <span> 💒</span>}
+                    <p className="mt-1 mb-0">Invited Guests:</p>
+                    {invitee.invitedGuests.map((guest, index) => {
+                      return (
+                        <p className="mb-0" key={index}>
+                          {guest}
+                        </p>
+                      );
+                    })}
+                  </td>
+                  <td className="text-end">
+                    {invitee.attendance}
+                    {invitee.attendance === "in-person" && (
+                      <>
+                        <p className="mt-1 mb-0">Confirmed Guests:</p>
+                        {invitee.confirmedGuests.map((guest, index) => {
+                          return (
+                            <p className="mb-0" key={index}>
+                              {guest}
+                            </p>
+                          );
+                        })}
+                      </>
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+        </Container>
+      </div>
     </PageLayout>
   );
 };
